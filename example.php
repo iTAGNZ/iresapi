8a14e993409d6acbd2962c670a0ea666a130cc51
Robbie Averill <robbie@averill.co.nz>
2013-12-18 10:51:08 +1300

    [#62602310] started demo scripts for API v1

---
+++ example.php
@@ -0,0 +1,89 @@
+<?php
+
+//
+// There are many different ways to make HTTP requests with PHP. The appropriate solution will depend on your PHP version.
+// in this example, we will use CURL by default.
+// - have a look at lib/api.php for examples of how to make requests in other routes (e.g. if CURL is not available on your system)
+//
+// Please note that these examples have error reporting for most cases where an API call could possibly fail. This may be overkill for
+// a practical application, but outlines what steps can be done to validate requests.
+//
+// Successful requests will return header code 200 OK, where as various errors will return various other codes. Errors will still return
+// data containing information about the error, but you may encounter a boolean false result from the call instead of error data. In this
+// case, e.g. with stream_context_create(), you will need to specify 'ignore_errors' => true.
+//
+
+require_once('lib/api.php');
+
+// pass your API token into the constructor of iRESAPI class
+$api = new iRESAPI('test');
+
+// For testing purposes: throw exception when header response code is not 200? 
+// If this is false, output will be shown at the bottom of the page.
+# $api->die_on_error = true;
+
+// optional: define filtering options (refer to the docs)
+$api->limit = 5;
+# $api->order = 'Contact DESC';
+
+try {
+
+	// --- CURL
+	# $operators = $api->get_operators_curl();
+	// --- PHP Streams
+	$operators = $api->get_operators_stream('GET');
+	
+	// Note: - the PECL_HTTP extension must be loaded on your server for these methods to work
+	// 	     - header response checks have not been added for these options, so you will need to add your own.
+	// --- PECL_HTTP (procedural)
+	# $operators = $api->get_operators_pecl();
+	// --- PECL_HTTP (OO)
+	# $operators = $api->get_operators_pecl_oo();
+
+} catch (Exception $e) { ?>
+	<pre><span style="color: red">API Error:</span>
+<?=$e?>
+<span style="color: red">Attempting header trace:</span>
+<?php 
+	print_r($api->response_headers);
+	exit;
+}
+
+// return is JSON by default
+$results = json_decode($operators, true);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>iRES API v1 - Example</title>
+	<meta name="description" content="iRES API v1 - Example">
+	<meta name="author" content="iRES">
+</head>
+
+<body>
+	
+	<h1>iRES API v1 - Example</h1>
+	<p>This page shows you examples of how to connect to iRES API and retrieve information. <a href="https://github.com/robbieaverill/iresapi" target="_blank">For documentation, please click here.</a></p>
+	
+	<h2>Operator results:</h2>
+	<ul class="results"><?php
+		if($api->response_code != 200)
+			echo 'API request failed... Skipping results';
+		else {
+			foreach($results as $operator) {
+				echo "\n\t\t"; // spacing for source code
+				echo '<li><span style="font-weight: bold">' . $operator['name'] . '</span> - Contact: ' . $operator['contact'] . ' on ' . $operator['phone'] . '</li>';
+			}
+			echo "\n";
+		}
+		?>
+	</ul>
+	
+	<h3>Data returned from API</h3>
+	<div style="height: 300px; background-color: #eee; border: 1px solid #ccc; overflow: scroll">
+		<pre><?php print_r($operators); ?></pre>
+	</div>
+	
+</body>
+</html>
---
+++ lib/api.php
@@ -0,0 +1,179 @@
+<?php
+/*
+  ------------------------------------------------
+  ------ Demonstration code for iRES API v1 ------
+  ------------------------------------------------
+*/
+
+class iRESAPI {
+
+	// define API URL (format: [server]/api/[version])
+	public $url = 'https://ires.co.nz/api/v1';
+	
+	// define default search options
+	public $limit = 30;
+	public $order = 'name ASC';
+	
+	private $api_url = '';
+	private $action = '';
+	private $api_token = '';
+	private $default_return = 'JSON';
+	private $data_to_send = '';
+	
+	public $response_headers = null;
+	public $response_code = null;
+	public $die_on_error = false;
+	
+	// instantiate required variables
+	public function __construct($api_token = null) {
+		if(!$api_token)
+			die('You must provide an API token!');
+			
+		// Set the required API token. Your credentials for iRES resource access will be
+		// automatically calculated from this API token.
+		$this->api_token = $api_token;
+	}
+	
+	// --- Get operators: CURL option
+	// http://php.net/curl
+	public function get_operators_curl() {
+		$this->set_api_url('operators');
+		$curl = curl_init($this->api_url);
+		if(!$curl)
+			throw new Exception('Cannot open CURL.');
+		
+		// set options for CURL
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $this->get_data());
+		curl_setopt($curl, CURLOPT_HEADER, 1); // return response header code
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Expect:')); // disable the "100 Continue" header
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		
+		$response = curl_exec($curl);
+		if(!$response)
+			throw new Exception('Could not get response from CURL.');
+		
+		list($headers, $body) = explode("\r\n\r\n", $response, 2); // split headers and body
+		$this->response_headers = explode("\n", $headers); // split headers into array by new lines
+		// throw exception when header is incorrect
+		$this->check_response_headers();
+
+		curl_close($curl);
+		return $body;
+	}
+	
+	// --- Get operators: Stream option
+	// http://php.net/stream_context_create
+	public function get_operators_stream($method = 'POST') {
+		$this->set_api_url('operators');
+		
+		// params for stream
+		$params = http_build_query($this->get_data());
+		
+		// initiate stream
+		$send_params = array(
+			'http' => array(
+				'method' => $method,
+				'ignore_errors' => true
+			)
+		);
+		
+		// set variables appropriately
+		if($method == 'POST')
+			$send_params['http']['content'] = $params;
+		else
+			$this->api_url .= '?' . $params;
+		
+		// get and save headers
+		stream_context_set_default($send_params);
+		$headers = @get_headers($this->api_url, 1);
+		$this->response_headers = $headers;
+		
+		// throw exception when header is incorrect
+		$this->check_response_headers();
+		
+		// create request
+		$request = stream_context_create($send_params);
+		
+		// make request to API
+		$fp = @fopen($this->api_url, 'rb', false, $request);
+		if(!$fp)
+			throw new Exception('Stream: Error opening URL.');
+			
+		// get results
+		$response = @stream_get_contents($fp);
+		if($response === false)
+			throw new Exception('Stream: Error getting response.');
+		
+		return $response;
+	}
+	
+	// --- Get operators: PECL_HTTP option (procedural, requires PECL_HTTP enabled)
+	// http://www.php.net/manual/en/function.http-post-data.php
+	public function get_operators_pecl() {
+		$this->action = 'operators';
+		
+		// check that extension is enabled
+		if(!extension_loaded('pecl_http'))
+			throw new Exception('PECL_HTTP extension is not loaded on this server.');
+		
+		// everything is performed with one line
+		$response = http_post_data($this->api_url, $this->get_data());
+		if(!$response)
+			throw new Exception('PECL: Error processing http_post_data request.');
+			
+		return $response;
+	}
+	
+	// --- Get operators: PECL_HTTP option (OO, requires PECL_HTTP enabled)
+	// http://www.php.net/manual/en/class.httprequest.php
+	public function get_operators_pecl_oo() {
+		$this->action = 'operators';
+		
+		// check that extension is enabled
+		if(!extension_loaded('pecl_http'))
+			throw new Exception('PECL_HTTP extension is not loaded on this server.');
+		
+		$request = new HTTPRequst($this->api_url, HTTP_METH_POST);
+		if(!$request)
+			throw new Exception("Error instantiating HTTPRequest class to $this->api_url");
+			
+		$request->setRawPostData($this->get_data());
+		$request->send();
+		
+		$response = $request->getResponseBody();
+		if(!$response)
+			throw new Exception("Error getting a response from HTTPRequest: $this->api_url");
+		return $response;
+	}
+	
+	private function get_data() {
+		// Define data to send via appropriate HTTP method
+		return array(
+			'token' => $this->api_token
+			// these are optional parameters, refer to the docs
+			,'order' => $this->order
+			,'limit' => $this->limit
+		);
+	}
+	
+	private function set_api_url($action) {
+		$this->action = $action;
+		$this->api_url = $this->url  . '/' . $action;
+	}
+
+	// Check response headers for 200, throw exception when otherwise
+	private function check_response_headers() {
+		$code = $this->get_http_response_code($this->response_headers);
+		$this->response_code = (int)$code;
+		if((int)$code != 200 && $this->die_on_error)
+			throw new Exception('Error: response header is: ' . $this->response_headers[0]);
+	}
+	
+	// Return response code from headers, e.g. 405 from HTTP/1.0 405 Method Not Allowed
+	public function get_http_response_code($response_headers) {
+		return substr($response_headers[0], 9, 3);
+	}
+}
+
+?>
